b8b6b18ac5017aec5c9944b3cd9f2fe04d32adce
夏遇泽 <2132047479@qq.com>
2024-10-08 16:52:05 +0800

    优化模型

--- client/model/model.go
+++ client/model/model.go
@@ -1,15 +1,12 @@
 package model
 
 import (
-	"github.com/go-redis/redis"
 	"github.com/shopspring/decimal"
 	"time"
 )
 
-var RDB *redis.Client
-
 type Sellers struct {
-	ID       uint       `json:"id" gorm:"primarykey"`
+	SellerID uint       `json:"id" gorm:"primaryKey"`
 	Name     string     `json:"name" gorm:"size:255;unique;not null"`
 	Password string     `json:"password" gorm:"size:255;not null"`
 	Activity Activities `gorm:"foreignkey:ActivityID"`
@@ -17,27 +14,29 @@ type Sellers struct {
 }
 
 type Items struct {
-	ItemID uint64          `gorm:"primarykey"`
+	ItemID uint64          `gorm:"primaryKey"`
+	Name   string          `json:"name" gorm:"size:255;unique;not null"`
 	Price  decimal.Decimal `json:"price" gorm:"type:decimal(10,2)"`
 }
 
 type Buyers struct {
-	BuyerID  uint   `gorm:"primarykey"`
+	BuyerID  uint   `gorm:"primaryKey"`
 	Name     string `gorm:"type:varchar(20);not null"`
 	Password string `gorm:"type:varchar(255);not null"`
+	Token    string `gorm:"type:varchar(255);not null"`
 	Order    Orders `gorm:"foreignkey:OrderID"`
 }
 
 type Activities struct {
-	ActivityID uint      `gorm:"primarykey"`
-	PosterID   uint      `json:"poster_id"`
+	ActivityID uint      `gorm:"primaryKey"`
+	SellerID   uint      `gorm:"foreignkey:SellerID"`
 	Left       uint      `json:"left"`
 	LastTime   time.Time `json:"last_time"`
 	Item       Items     `gorm:"foreignkey:ItemID"`
 }
 
 type Orders struct {
-	OrderID  uint      `gorm:"primarykey"`
+	OrderID  uint      `gorm:"primaryKey"`
 	BuyerID  uint      `json:"buyer_id"`
 	SellerID uint      `json:"seller_id"`
 	Status   bool      `json:"status"`
