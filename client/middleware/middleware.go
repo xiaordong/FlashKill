4a10f7d39fef094c3288f8a329dbc70ad5e5518a
夏遇泽 <2132047479@qq.com>
2024-10-10 20:45:33 +0800

    限流中间件

---
+++ client/middleware/middleware.go
@@ -0,0 +1,65 @@
+package middleware
+
+import (
+	"client/resp"
+	"context"
+	"github.com/cloudwego/hertz/pkg/app"
+	"sync"
+	"time"
+)
+
+// TokenBucket 令牌桶算法
+type TokenBucket struct {
+	capacity int64 //最大容量，及分发的最大令牌数（==实际商品数）
+	rate     int64 //每秒分发的速度
+	tokens   int64
+	mutex    sync.Mutex
+}
+
+func NewTokenBucket(capacity, rate int64) *TokenBucket {
+	tb := &TokenBucket{
+		capacity: capacity,
+		rate:     rate,
+		tokens:   capacity,
+	}
+	go tb.fill()
+	return tb
+}
+
+func (tb *TokenBucket) fill() {
+	lastTime := time.Now()
+	for range time.Tick(time.Second) {
+		tb.mutex.Lock()
+		elapsed := time.Since(lastTime).Seconds()
+		lastTime = time.Now()
+		addedTokens := int64(elapsed * float64(tb.rate))
+		if addedTokens > 0 {
+			tb.tokens += addedTokens
+			if tb.tokens > tb.capacity {
+				tb.tokens = tb.capacity
+			}
+		}
+		tb.mutex.Unlock()
+	}
+}
+
+func (tb *TokenBucket) Take() bool {
+	tb.mutex.Lock()
+	defer tb.mutex.Unlock()
+	if tb.tokens > 0 {
+		tb.tokens--
+		return true
+	}
+	return false
+}
+
+func RLMiddleware(tb *TokenBucket) app.HandlerFunc {
+	return func(c context.Context, ctx *app.RequestContext) {
+		if !tb.Take() {
+			resp.Response(ctx, resp.WithCode(503), resp.WithMsg("too many requests"))
+			ctx.Abort()
+			return
+		}
+		ctx.Next(c)
+	}
+}
